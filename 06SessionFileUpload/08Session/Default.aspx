8b131c129b68eeb9fded2ff01c8339490436b07a
shalin729 <shalin.bhanat29@gmail.com>
2025-01-24 22:22:28 +0530

    create session file upload

---
+++ 06SessionFileUpload/08Session/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Delete" />
+        <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        <br />
+        <br />
+        Img 1 <br />
+        Title =
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label><br />
+        Size =
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        <br />
+       
+       
+        <br />
+        <br />
+        <asp:Image ID="Image1" runat="server" Height="140px" Width="240px" />
+        <asp:Image ID="Image2" runat="server" Height="140px" Width="240px" />
+        <asp:Image ID="Image3" runat="server" Height="140px" Width="240px" />
+        <asp:Image ID="Image4" runat="server" Height="140px" Width="240px" />
+        <asp:Image ID="Image5" runat="server" Height="140px" Width="240px" />
+        <br />
+        <br />
+    </form>
+</body>
+</html>
---
+++ 06SessionFileUpload/08Session/Default.aspx.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    
+        static int i = 1;
+    
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        
+
+        if (FileUpload1.HasFile)
+        {
+            
+
+                int size = Convert.ToInt32(FileUpload1.FileContent.Length) / 1024;
+        switch (i)
+        {
+            case 1:
+                if(size < 200)
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("~/upload/" + FileUpload1.FileName));
+                        Image1.ImageUrl = "~/upload/" + FileUpload1.FileName;
+
+                        Label1.Text = FileUpload1.FileName + " succcessfully attached...";
+
+                        Label2.Text = (size) + "KB";
+                    }
+                    else
+                    {
+                        Response.Write("size must be under 200kb");
+                        i--;
+                    }
+                
+                break;
+            case 2:
+                    //int size = Convert.ToInt32(FileUpload1.FileContent.Length) / 1024;
+                    if (size < 200)
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("~/upload/" + FileUpload1.FileName));
+                        Image2.ImageUrl = "~/upload/" + FileUpload1.FileName;
+
+                        Label1.Text = FileUpload1.FileName + " succcessfully attached...";
+
+                        Label2.Text = (size) + "KB";
+                    }
+                    else
+                    {
+                        Response.Write("size must be under 200kb");
+                        i--;
+                    }
+                    break;
+                
+            case 3:
+                    if (size < 200)
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("~/upload/" + FileUpload1.FileName));
+                        Image3.ImageUrl = "~/upload/" + FileUpload1.FileName;
+
+                        Label1.Text = FileUpload1.FileName + " succcessfully attached...";
+
+                        Label2.Text = (size) + "KB";
+                    }
+                    else
+                    {
+                        Response.Write("size must be under 200kb");
+                        i--;
+                    }
+                    break;
+            case 4:
+                    if (size < 200)
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("~/upload/" + FileUpload1.FileName));
+                        Image4.ImageUrl = "~/upload/" + FileUpload1.FileName;
+
+                        Label1.Text = FileUpload1.FileName + " succcessfully attached...";
+
+                        Label2.Text = (size) + "KB";
+                    }
+                    else
+                    {
+                        Response.Write("size must be under 200kb");
+                        i--;
+                    }
+                    break;
+            case 5:
+                    if (size < 200)
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("~/upload/" + FileUpload1.FileName));
+                        Image5.ImageUrl = "~/upload/" + FileUpload1.FileName;
+
+                        Label1.Text = FileUpload1.FileName + " succcessfully attached...";
+
+                        Label2.Text = (size) + "KB";
+                    }
+                    else
+                    {
+                        Response.Write("size must be under 200kb");
+                        i--;
+                    }
+                    break;
+            default:
+            break;
+
+        }
+            i++;
+
+            if (i > 5)
+            {
+                Response.Write("You can not add more then 5 file");
+                i = 5;
+            }
+
+        }
+        else
+        {
+            Response.Write("pls select file");
+        }
+    }
+
+
+   
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        if (Image1.ImageUrl != "")
+        {
+            File.Delete(Server.MapPath(Image1.ImageUrl));
+            Image1.ImageUrl = "";
+        }
+        else if (Image2.ImageUrl != "")
+        {
+            File.Delete(Server.MapPath(Image2.ImageUrl));
+            Image2.ImageUrl = "";
+        }
+        else if (Image3.ImageUrl != "")
+        {
+            File.Delete(Server.MapPath(Image3.ImageUrl));
+            Image3.ImageUrl = "";
+        }
+        else if (Image4.ImageUrl != "")
+        {
+            File.Delete(Server.MapPath(Image4.ImageUrl));
+            Image4.ImageUrl = "";
+        }
+        else if (Image5.ImageUrl != "")
+        {
+            File.Delete(Server.MapPath(Image5.ImageUrl));
+            Image5.ImageUrl = "";
+        }
+
+        i--;
+        if (i >= 1)
+        {
+            i = 1;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
