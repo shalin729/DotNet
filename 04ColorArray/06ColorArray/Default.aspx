6c1733c0f2473e6bc02c75cf488483cb851f7e7c
shalin729 <shalin.bhanat29@gmail.com>
2025-01-24 10:21:31 +0530

    create color array

---
+++ 04ColorArray/06ColorArray/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body id="bd1" runat="server">
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Change bg Color" />
+        </div>
+        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+            <asp:ListItem Value="blue"></asp:ListItem>
+            <asp:ListItem>green</asp:ListItem>
+            <asp:ListItem Value="brown"></asp:ListItem>
+            <asp:ListItem Value="pink"></asp:ListItem>
+            <asp:ListItem Value="red"></asp:ListItem>
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ 04ColorArray/06ColorArray/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+
+    }
+
+   static string[,] colors = { {"red", "blue", "green" }, {"black", "#efddd4","#FF6633" }, { "red", "blue", "green" }, { "black", "#efddd4", "#FF6633" }, { "red", "blue", "green" }, { "black", "#efddd4", "#FF6633" }, { "red", "blue", "green" }, { "black", "#efddd4", "#FF6633" } };
+
+    static int i = 0;
+    static int j = 0;
+ 
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        //i++;
+        bd1.Style.Add("background-color", colors[i,j]);
+        j++;
+        if (j == 3) { i++; j = 0; }
+        Response.Write(i);
+
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        bd1.Style.Add("background-color", DropDownList1.SelectedValue);
+
+    }
+
+}
