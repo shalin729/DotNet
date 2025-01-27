99bb8ece9cc8bc736cb7e90e72edc433b97ef814
shalin729 <shalin.bhanat29@gmail.com>
2025-01-27 08:59:25 +0530

    create like app using cookie

---
+++ 10Cookie_Like/09Cookie_Like/Like.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Like.aspx.cs" Inherits="Like" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Like" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Dislike" />
+            
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+&nbsp; Likes</form>
+</body>
+</html>
---
+++ 10Cookie_Like/09Cookie_Like/Like.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Like : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (Request.Cookies["likes"] != null)
+        {
+            Label1.Text = Request.Cookies["likes"].Values["v1"];
+        }else
+        { 
+            Response.Write("Not set"); 
+        }
+
+        //string likecount = Request.Cookies["likes"].Values["like"];
+        //Label1.Text = i.ToString();
+        //Label1.Text = like.Value;
+
+
+    }
+    static int i = 0;
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        i = i + 1;
+        //Label1.Text = i.ToString();
+        HttpCookie like = new HttpCookie("likes");
+        like.Values.Add("v1", i.ToString());
+        like.Expires = DateTime.Now.AddDays(30);
+        Response.Cookies.Add(like);
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        i--;
+        //Label1.Text = i.ToString();
+        HttpCookie like = new HttpCookie("likes");
+        like.Values.Add("v1",i.ToString());
+        like.Expires = DateTime.Now.AddDays(30);
+        Response.Cookies.Add(like);
+  
+    }
+
+
+}
+
+
