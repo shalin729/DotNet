4e3096a8c6501805e4b80d3eb3e371ac58005933
shalin729 <shalin.bhanat29@gmail.com>
2025-02-10 22:07:54 +0530

    create bitmap

---
+++ 30_BitMap/30_BitMap/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server" Height="148px" Width="487px"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="gif" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="png" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="jpg" />
+    </form>
+</body>
+</html>
---
+++ 30_BitMap/30_BitMap/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Bitmap b = new Bitmap(300, 300, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        Graphics g = Graphics.FromImage(b);
+        string Text;
+        g.Clear(Color.Red);
+        Text = TextBox1.Text;
+        g.DrawString(Text, new Font("Arial", 12),
+        new SolidBrush(Color.Blue), new PointF(0.5F, 2.5F));
+        Response.ContentType = "image/gif";
+        b.Save(Response.OutputStream, ImageFormat.Gif);
+        b.Dispose();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Bitmap b = new Bitmap(300, 300, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        Graphics g = Graphics.FromImage(b);
+        string Text;
+        g.Clear(Color.Red);
+        Text = TextBox1.Text;
+        g.DrawString(Text, new Font("Arial", 12),
+        new SolidBrush(Color.Blue), new PointF(0.5F, 2.5F));
+        Response.ContentType = "image/png";
+        b.Save(Response.OutputStream, ImageFormat.Gif);
+        b.Dispose();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Bitmap b = new Bitmap(300, 300, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        Graphics g = Graphics.FromImage(b);
+        string Text;
+        g.Clear(Color.Red);
+        Text = TextBox1.Text;
+        g.DrawString(Text, new Font("Arial", 12),
+        new SolidBrush(Color.Blue), new PointF(0.5F, 2.5F));
+        Response.ContentType = "image/jpg";
+        b.Save(Response.OutputStream, ImageFormat.Gif);
+        b.Dispose();
+    }
+}
